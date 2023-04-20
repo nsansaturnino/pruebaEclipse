d1f69a743fbd523f6b4f3213d77123a2e8e99ee3
Nora <nsansaturnino@birt.eus>
2023-04-20 12:22:30 +0200

    Primeros cambios

--- src/eclipseGit/Main.java
+++ src/eclipseGit/Main.java
@@ -1,9 +1,10 @@
 package eclipseGit;
 
+// Comentarrios
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
 	}
 
