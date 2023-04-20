f33da0d0a9b48b8194bd3d5c404978df7e338ba8
nsansaturnino <51241037+nsansaturnino@users.noreply.github.com>
2023-04-20 12:56:01 +0200

    Update Main.java

--- src/eclipseGit/Main.java
+++ src/eclipseGit/Main.java
@@ -1,11 +1,9 @@
 package eclipseGit;
 
 // Comentarios
-// Más comentarios
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hola mundo");
 		System.out.println("Hola mundo");
 	}
 
